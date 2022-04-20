164fafd8987c18a9ccab8ec6dc3d019ab14df4eb
lmonsalve22 <lmonsalve22@gmail.com>
2022-04-20 17:51:43 -0400

    Update automatizacion_estructurado.py

--- automatizacion_estructurado.py
+++ automatizacion_estructurado.py
@@ -36,6 +36,75 @@ def getPrueba():
     df.to_excel("text.xlsx")
     print(df)
 
+def getData(codigo, password):
+    #print(codigo)
+    url = f"https://si3.bcentral.cl/SieteRestWS/SieteRestWS.ashx?user=169073872&pass={password}&firstdate=2000-01-01&lastdate=2021-12-31&timeseries={codigo}&function=GetSeries"
+    response = requests.get(url)    
+    response1 = response.json()
+    response2 = response1["Series"]["Obs"]
+    #return pd.DataFrame(response2) #.to_csv(f"{codigo}.csv", index=False)
+    df = pd.DataFrame(response2)
+    df["Codigo"] = codigo
+    return df
+
+def DescargaMasiva():
+    serie = getSeries()
+    password = getPass()
+    fechas = []
+    start =  datetime.datetime(2000,1,1)
+    now = datetime.datetime.now()
+    actual = start
+    while actual < now:
+        fechas.append(actual.strftime("%d-%m-%Y"))
+        actual = actual +  datetime.timedelta(days = 1)
+        referenciaFecha = pd.DataFrame({"Fecha":fechas})
+    diario = referenciaFecha
+    mensual = referenciaFecha
+    trimestral = referenciaFecha
+    anual = referenciaFecha
+    error = []
+    n = 1
+    for i in serie["Código"]:
+        frecuencia = i.split(".")[-1]
+        #print(frecuencia)
+        df = getData(i,password)
+        if(len(df) == 0):
+            error.append(i)
+        else:
+            print(i)
+            print(f"Llevamos {n} de {len(serie)}")
+            n = n + 1
+            if(frecuencia == "D"):        
+                diario = diario.merge(df, left_on="Fecha", right_on="indexDateString", how="left")
+                diario[i] = diario["value"] 
+                del diario["value"]
+                del diario["statusCode"]
+                del diario["Codigo"]
+            elif(frecuencia == "M"):
+                mensual = mensual.merge(df, left_on="Fecha", right_on="indexDateString", how="left")
+                mensual[i] = mensual["value"] 
+                del mensual["value"]
+                del mensual["statusCode"]
+                del mensual["Codigo"]
+            elif(frecuencia == "T"):
+                trimestral = trimestral.merge(df, left_on="Fecha", right_on="indexDateString", how="left")
+                trimestral[i] = trimestral["value"] 
+                del trimestral["value"]
+                del trimestral["statusCode"]
+                del trimestral["Codigo"]
+            elif(frecuencia == "T"):
+                anual = anual.merge(df, left_on="Fecha", right_on="indexDateString", how="left")
+                anual[i] = anual["value"] 
+                del anual["value"]
+                del anual["statusCode"]
+                del anual["Codigo"]
+    diario.to_excel("dataEstructurado/diario1.xlsx", index=False)
+    mensual.to_excel("dataEstructurado/mensual1.xlsx", index=False)
+    trimestral.to_excel("dataEstructurado/trimestral1.xlsx", index=False) 
+    anual.to_excel("dataEstructurado/anual1.xlsx", index=False)
+    return
+
+
 def getData(codigo):
     #print(codigo)
     #print(1)
@@ -78,5 +147,4 @@ def proceso():
 
 if __name__ == '__main__':
     print("Comenzo...")
-    getPrueba()
-    print(getSeries())
+    DescargaMasiva()
